9be549c8ed6e2f884b4a1151984d1286d4991180
Pranav Kant <pranav913@gmail.com>
2018-11-17 20:08:38 -0700

    compiles the complex example now

--- src/llvm-ai.cpp
+++ src/llvm-ai.cpp
@@ -1,12 +1,22 @@
 #include <unordered_map>
+#include <queue>
 
 #include "llvm/IR/ConstantRange.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
+
+enum class LatticeVal : char {
+    // don't need any Top because ConstantRange will be limited
+    // by its bitwidth.
+    ConstantRange,
+    Bottom,
+};
 
 class AIPass : public FunctionPass {
 public:
@@ -14,56 +24,181 @@ public:
     AIPass() : FunctionPass(ID) {}
 
     bool runOnFunction(Function& F) override {
-	for (auto& BB : F) {
-	    for (auto& I : BB) {
-		if (!I.getType()->isIntegerTy())
-		    continue;
+	// create a worklist
+	std::queue<Value*> worklist = init(F);
+	while (!worklist.empty()) {
+	    Value* val = worklist.front(); worklist.pop();
 
-		if (!I.isBinaryOp())
-		    continue;
+	    if (isa<Instruction>(val))
+		outs() << *cast<Instruction>(val) << "\n";
 
-		IntegerType* intType = dyn_cast<IntegerType>(I.getType());
-		assert(intType);
-
-		const unsigned bitWidth = intType->getBitWidth();
-		Value* v1 = I.getOperand(0);
-		Value* v2 = I.getOperand(1);
-		const ConstantRange& r1 = getRange(v1, bitWidth);
-		const ConstantRange& r2 = getRange(v2, bitWidth);
-		ConstantRange result(bitWidth, true);
-
-		switch(I.getOpcode()) {
-		case Instruction::Add:
-		    result = r1.add(r2);
-		    break;
-		case Instruction::Sub:
-		    result = r1.sub(r2);
-		    break;
-		default:
-		    break;
+	    std::pair<LatticeVal, ConstantRange*> oldres = getCurrentValue(val);
+	    std::pair<LatticeVal, ConstantRange*> newres = processValue(val);
+	    if (oldres.second != newres.second) {
+		// delete any existing ConstantRanges, if any
+		if (_mapRanges.find(val) != _mapRanges.find(val)) {
+		    if (_mapRanges.at(val) != nullptr) {
+			delete _mapRanges.at(val);
+		    }
 		}
 
-		_map.emplace(dyn_cast<Value>(&I), result);
-		outs() << "Instruction: " << I
-		       << " , CR: " << result << "\n";
+		_mapLatticeVals[val] = newres.first;
+		_mapRanges[val] = newres.second;
+
+		// TODO: put all dependents in the worlist,
+		// only if they are not already in
+		for (auto user : val->users()) {
+		    worklist.push(cast<Value>(user));
+		}
 	    }
 	}
+
+
+
+	return false;
     }
 private:
-    std::unordered_map<Value*, ConstantRange> _map;
+    // contains some value of the lattice, LatticeVal
+    std::unordered_map<Value*, LatticeVal> _mapLatticeVals;
+
+    // If _mapLatticeVals of some Value* is LatticeVal::ConstantRange, the
+    // actual constant range is stored here
+    std::unordered_map<Value*, ConstantRange*> _mapRanges;
 
 private:
-    // Returns the already calculated range for val
-    // If not exists, create a new range of width = bitWidth
-    const ConstantRange& getRange(Value* val, const unsigned bitWidth) {
-	if (_map.find(val) == _map.end()) {
-	    if (auto constInt = dyn_cast<ConstantInt>(val))
-		_map.emplace(val, constInt->getValue());
-	    else
-		_map.emplace(val, ConstantRange(bitWidth, true)); // full-set
+    std::pair<LatticeVal, ConstantRange*> processValue(Value* val) {
+	std::pair<LatticeVal, ConstantRange*> result;
+
+	// we know nothing about non-integers!!!
+	if (!val->getType()->isIntegerTy())
+	    return {LatticeVal::Bottom, nullptr};
+
+	unsigned bitWidth = val->getType()->getIntegerBitWidth();
+	outs() << "processValue: bitWidth: " << bitWidth << "\n";
+
+	if (auto *fnarg = dyn_cast<Argument>(val)) {
+	    outs() << "processValue: is a fn arg: " << "\n";
+	    outs() << fnarg->getParent() << "\n";
+	    return {LatticeVal::ConstantRange,
+		    new ConstantRange(bitWidth, true)};
+	}
+	else if (auto* cint = dyn_cast<ConstantInt>(val)) {
+	    outs() << "processValue: is a const inst: " << "\n";
+	    return {LatticeVal::ConstantRange,
+		    new ConstantRange(cint->getValue())};
+	}
+	else if (auto* callinst = dyn_cast<CallInst>(val)) {
+	    outs() << "processValue: is a call inst: " << "\n";
+	    return {LatticeVal::ConstantRange,
+		    new ConstantRange(bitWidth, true)};
+	}
+	else if (auto* inst = dyn_cast<Instruction>(val)) {
+	    outs() << "processValue: xfunc\n";
+	    return xfunc(val);
 	}
 
-	return _map.at(val);
+	outs() << "nothing processed\n";
+    }
+
+    // execute appropriate transfer function on val
+    std::pair<LatticeVal, ConstantRange*> xfunc(Value* val) {
+	Instruction* inst = cast<Instruction>(val);
+	assert(inst);
+
+	std::pair<LatticeVal, ConstantRange*> defaultpair =
+	    {LatticeVal::Bottom,
+	     nullptr};
+
+	if (!val->getType()->isIntegerTy())
+	    return defaultpair;
+
+	// TODO: Extend to non-binary ops transfer functions too
+	if (!inst->isBinaryOp())
+	    return defaultpair;
+
+	const unsigned bitWidth = val->getType()->getIntegerBitWidth();
+	Value* v1 = inst->getOperand(0);
+	Value* v2 = inst->getOperand(1);
+	auto res1 = getCurrentValue(v1);
+	auto res2 = getCurrentValue(v2);
+	if (res1.first == LatticeVal::Bottom ||
+	    res2.first == LatticeVal::Bottom)
+	    return defaultpair;
+
+	const ConstantRange& r1 = *res1.second;
+	const ConstantRange& r2 = *res2.second;
+	ConstantRange result(bitWidth, true);
+
+	switch(inst->getOpcode()) {
+	case Instruction::Add:
+	    result = r1.add(r2);
+	    return {LatticeVal::ConstantRange,
+		    new ConstantRange(result)};
+	    break;
+	case Instruction::Sub:
+	    result = r1.sub(r2);
+	    return {LatticeVal::ConstantRange,
+		    new ConstantRange(result)};
+	    break;
+	default:
+	    return defaultpair;
+	}
+    }
+
+    std::pair<LatticeVal, ConstantRange*> getCurrentValue(Value* val) {
+	assert(_mapLatticeVals.find(val) != _mapLatticeVals.end());
+	assert(_mapRanges.find(val) != _mapRanges.end());
+
+	return {_mapLatticeVals.at(val), _mapRanges.at(val)};
+    }
+
+    // Initialize all values -- assign appropriate lattice elements, etc.
+    std::queue<Value*> init(Function& F) {
+        // if you don't know about something, init to full range
+	// otherwise, it's a constant
+	auto fnIterator = F.arg_begin();
+	while (fnIterator != F.arg_end()) {
+	    Value* arg = fnIterator;
+
+	    std::pair<LatticeVal, ConstantRange*> res = processValue(arg);
+	    _mapLatticeVals[arg] = res.first;
+	    _mapRanges[arg] = res.second;
+
+	    fnIterator++;
+	}
+
+	std::queue<Value*> worklist;
+	outs() << "initing\n";
+	for (auto& BB : F) {
+	    for (auto& I : BB) {
+		outs() << I << "\n";
+		Value* val = cast<Value>(&I);
+
+		std::pair<LatticeVal, ConstantRange*> res = processValue(val);
+		_mapLatticeVals[val] = res.first;
+
+		// delete any existing constant ranges
+		if (_mapRanges.find(val) != _mapRanges.end()) {
+		    if (_mapRanges.at(val) != nullptr)
+			delete _mapRanges.at(val);
+		}
+		_mapRanges[val] = res.second;
+
+		worklist.push(val);
+	    }
+	}
+
+	return worklist;
+    }
+
+    void dumpAnalysis() {
+	for (auto& val : _mapRanges) {
+	    if (!isa<Instruction>(val.first))
+		continue;
+	    outs() << *cast<Instruction>(val.first) << "\n";
+	    outs() << "\tResult: "
+		   << val.second << "\n";
+	}
     }
 };
 
